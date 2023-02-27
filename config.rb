272bacc31b67c6336c49615b3b5a56dbbaa17882
AlexMachariaMaranga <amaranga22@gmail.com>
2023-02-27 13:49:39 +0300

    updated readme

--- config.rb
+++ config.rb
@@ -3,17 +3,17 @@ require_relative './authors.rb'
 require_relative './article.rb'
 require_relative './magazine.rb'
 
-author1 = Author.new('J.K. Rowling')
-author2 = Author.new('Stephen King')
+author1 = Author.new('M.c Tricks')
+author2 = Author.new('Luthar Enomes')
 
 # Create magazines
-magazine1 = Magazine.new('TIME', 'News')
-magazine2 = Magazine.new('Rolling Stone', 'Music')
+magazine1 = Magazine.new('NEWTIMES', 'News')
+magazine2 = Magazine.new('BET AWARDS', 'Music')
 
 # Create articles
-article1 = Article.new(author1, magazine1, 'The Power of Love')
-article2 = Article.new(author1, magazine1, 'The Importance of Diversity')
-article3 = Article.new(author2, magazine2, 'The Evolution of Rock and Roll')
+article1 = Article.new(author1, magazine1, 'Microsoft Insider')
+article2 = Article.new(author1, magazine1, 'Cultural Shock')
+article3 = Article.new(author2, magazine2, 'The Evolution of Utopia')
 
 # Test object relationship methods
 p article1.author
@@ -23,7 +23,7 @@ p author1.magazines
 p magazine1.contributors
 
 # Test association and aggregate methods
-author1.add_article(magazine2, 'The Art of Horror')
+author1.add_article(magazine2, 'The Heroic Man')
 p author1.topic_areas
 p Magazine.find_by_name('Rolling Stone')
 p magazine1.article_titles
